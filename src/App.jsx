52fe406eaa7bfb345d56f287aeda5eb41205d4bc
Arpon Dutta <arpongen3094@gmail.com>
2025-07-29 00:46:40 +0530

    adeed the particles component

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Hero from './sections/Hero'
 import About from './sections/About'
 import DancehausScroll from './components/DancehausScroll'
 import { SmoothCursor } from './components/SmoothCursor'
+import ParticleTextEffect from './components/ui/ParticleTextEffect'
 
 
 
 
 const App = () => {
   return (
+    <div className='box w-full h-full '>
     <div className='container mx-auto max-w-7xl'>
       <SmoothCursor/>
       <Navbar/>
       <Hero/>      
       <About/>
       <DancehausScroll/>
+      <ParticleTextEffect/>
 
+    </div>
     </div>
   )
 }
